186d5ef217d59dd93feb70a4d65b77ef61107a64
dustinlo <dustin.k.lo@jpl.nasa.gov>
2022-03-04 12:37:23 -0800

    changed Pods to Deployments for scalability
    removed --docker flag from deploy.sh and destroy.sh because kubectl.docker and kubectl share the same symlink

--- hysds/deploy.sh
+++ hysds/deploy.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-command=kubectl
 mozart=0
 grq=0
 factotum=0
@@ -13,7 +12,6 @@ Usage:
   Deploys the HySDS cluster in Kubernetes (Elasticsearch, Rest API, RabbitMQ, Redis, etc.)
   $0 [--docker] [mozart] [grq] [--all]
   Options:
-    --docker : use if running Kubernetes on Docker for Desktop; kubectl vs kubectl.docker
     --all : deploy all HySDS resources (Mozart + GRQ + factotum)
     mozart : deploy Mozart cluster
     grq : deploy GRQ cluster
@@ -26,9 +24,6 @@ while [ "$1" != "" ]; do
   -h | --help)
     docstring # run docstring function
     exit 0
-    ;;
-  --docker)
-    command=kubectl.docker
     ;;
   -a | --all)
     mozart=1
@@ -56,24 +51,24 @@ if (($grq == 0 && $mozart == 0 && $factotum == 0)); then
   exit 1
 fi
 
-$command delete cm celeryconfig || true
-$command create cm celeryconfig --from-file ./configs/celeryconfig.py
+kubectl delete cm celeryconfig || true
+kubectl create cm celeryconfig --from-file ./configs/celeryconfig.py
 
-$command delete cm netrc || true
-$command create cm netrc --from-file ./configs/.netrc
+kubectl delete cm netrc || true
+kubectl create cm netrc --from-file ./configs/.netrc
 
-$command delete cm aws-credentials || true
-$command create cm aws-credentials --from-file ./configs/aws-credentials
+kubectl delete cm aws-credentials || true
+kubectl create cm aws-credentials --from-file ./configs/aws-credentials
 
 helm repo add elastic https://helm.elastic.co
 helm repo update
 
 if (($mozart == 1)); then
-  $command delete cm mozart-settings || true
-  $command create cm mozart-settings --from-file ./mozart/rest_api/settings.cfg
+  kubectl delete cm mozart-settings || true
+  kubectl create cm mozart-settings --from-file ./mozart/rest_api/settings.cfg
 
-  $command delete cm logstash-configs || true
-  $command create cm logstash-configs \
+  kubectl delete cm logstash-configs || true
+  kubectl create cm logstash-configs \
     --from-file=job-status=./mozart/logstash/job_status.template.json \
     --from-file=event-status=./mozart/logstash/event_status.template.json \
     --from-file=worker-status=./mozart/logstash/worker_status.template.json \
@@ -103,16 +98,16 @@ if (($mozart == 1)); then
   user_rules_grq=$(curl -s https://raw.githubusercontent.com/hysds/grq2/develop/config/user_rules_dataset.mapping)
   curl -X PUT -H 'Content-Type: application/json' 'http://127.0.0.1:9200/user_rules-grq?pretty' -d "${user_rules_grq}"
 
-  $command apply -f ./mozart/rest_api/deployment.yml
-  $command apply -f ./mozart/redis/deployment.yml
-  $command apply -f ./mozart/logstash/deployment.yml
-  $command apply -f ./mozart/rabbitmq/deployment.yml
-  $command apply -f ./ui/deployment.yml
+  kubectl apply -f ./mozart/rest_api/deployment.yml
+  kubectl apply -f ./mozart/redis/deployment.yml
+  kubectl apply -f ./mozart/logstash/deployment.yml
+  kubectl apply -f ./mozart/rabbitmq/deployment.yml
+  kubectl apply -f ./ui/deployment.yml
 fi
 
 if (($grq == 1)); then
-  $command delete cm grq2-settings || true
-  $command create cm grq2-settings --from-file ./grq/rest_api/settings.cfg
+  kubectl delete cm grq2-settings || true
+  kubectl create cm grq2-settings --from-file ./grq/rest_api/settings.cfg
 
   helm repo add elastic https://helm.elastic.co
   helm install --wait --timeout 150s grq-es elastic/elasticsearch --version 7.9.3 -f ./grq/elasticsearch/values-override.yml
@@ -127,33 +122,33 @@ if (($grq == 1)); then
     'http://127.0.0.1:9201/_ingest/pipeline/dataset_pipeline' -d "${ingest_pipeline}" >/dev/null
   echo "writing ingest pipeline"
 
-  $command apply -f ./grq/rest_api/deployment.yml
+  kubectl apply -f ./grq/rest_api/deployment.yml
 fi
 
 if (($factotum == 1)); then
   mkdir -p /private/tmp/data || true
-  $command apply -f ./minio/volume.yml
+  kubectl apply -f ./minio/volume.yml
   sleep 5
 
-  $command delete cm supervisord-orchestrator || true
-  $command create cm supervisord-orchestrator --from-file ./orchestrator/supervisord.conf
+  kubectl delete cm supervisord-orchestrator || true
+  kubectl create cm supervisord-orchestrator --from-file ./orchestrator/supervisord.conf
 
-  $command delete cm datasets || true
+  kubectl create cm datasets --from-file ./configs/datasets.json
+
+  kubectl delete cm supervisord-job-worker || true
+  kubectl create cm supervisord-job-worker --from-file ./factotum/supervisord.conf
+
+  kubectl delete cm supervisord-user-rules || true
+  kubectl create cm supervisord-user-rules --from-file ./user_rules/supervisord.conf
+
   if [[ ! -f "./configs/datasets.json" ]]; then
     cp ./configs/datasets.template.json ./configs/datasets.json
   fi
-  $command create cm datasets --from-file ./configs/datasets.json
 
-  $command delete cm supervisord-job-worker || true
-  $command create cm supervisord-job-worker --from-file ./factotum/supervisord.conf
+  kubectl apply -f ./factotum/deployment.yml
+  kubectl apply -f ./orchestrator/deployment.yml
+  kubectl apply -f ./user_rules/deployment.yml
 
-  $command delete cm supervisord-user-rules || true
-  $command create cm supervisord-user-rules --from-file ./user_rules/supervisord.conf
-
-  $command apply -f ./factotum/deployment.yml
-  $command apply -f ./orchestrator/deployment.yml
-  $command apply -f ./user_rules/deployment.yml
-
-  $command apply -f ./minio/deployment.yml
-  $command apply -f ./minio/post-setup.yml
+  kubectl apply -f ./minio/deployment.yml
+  kubectl apply -f ./minio/post-setup.yml
 fi
