724bd13651d8bb598f87f46ff86b87a4574af55b
Drew Meyers <drew.meyers@jpl.nasa.gov>
2022-04-20 10:16:13 -0700

    Move kubernetes_secret resources to secrets.tf

--- terraform/terraform-unity-sps-hysds-cluster/main.tf
+++ terraform/terraform-unity-sps-hysds-cluster/main.tf
@@ -9,26 +9,6 @@ resource "kubernetes_namespace" "unity-sps" {
   }
 }
 
-
-resource "kubernetes_secret" "container-registry" {
-  metadata {
-    name      = "container-registry"
-    namespace = kubernetes_namespace.unity-sps.metadata.0.name
-  }
-  data = {
-    ".dockerconfigjson" = jsonencode({
-      auths = {
-        "${var.container_registry_server}" = {
-          "username" = var.container_registry_username
-          "password" = var.container_registry_password
-          "auth"     = base64encode("${var.container_registry_username}:${var.container_registry_password}")
-        }
-      }
-    })
-  }
-  type = "kubernetes.io/dockerconfigjson"
-}
-
 provider "helm" {
   kubernetes {
     config_path = var.kubeconfig_filepath
