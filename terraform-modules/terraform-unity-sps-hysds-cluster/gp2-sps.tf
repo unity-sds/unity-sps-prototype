21c48d3db46727a487a26d02dd40b59b0b946a23
Gerald Manipon <pymonger@gmail.com>
2022-11-01 16:11:09 -0700

    provision gp2-sps storageclass

---
+++ terraform-modules/terraform-unity-sps-hysds-cluster/gp2-sps.tf
@@ -0,0 +1,23 @@
+resource "kubernetes_storage_class" "gp2-sps" {
+  metadata {
+    name = "gp2-sps"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+  storage_provisioner = "kubernetes.io/aws-ebs"
+  reclaim_policy      = "Delete"
+  parameters = {
+    type = "gp2"
+    fsType = "ext4"
+  }
+  allow_volume_expansion = true
+  mount_options = ["debug"]
+}
+
+resource "null_resource" "unset_default" {
+  depends_on = [kubernetes_storage_class.gp2-sps]
+  provisioner "local-exec" {
+    command = "kubectl patch storageclass gp2 -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"false\"}}}'"
+  }
+}
