87c78c451793c26cc02afbbadecda28fb5f9982c
Drew Meyers <drew.meyers@jpl.nasa.gov>
2022-12-05 07:49:04 -0800

    feat: Remove S3

--- terraform-modules/terraform-unity-sps-hysds-cluster/s3.tf
+++ terraform-modules/terraform-unity-sps-hysds-cluster/s3.tf
@@ -1,28 +1,28 @@
-locals {
-  source_s3_directory = abspath("${path.module}/../../dev_data/SOURCE_S3/")
-}
+# locals {
+#   source_s3_directory = abspath("${path.module}/../../dev_data/SOURCE_S3/")
+# }
 
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket        = "${var.project}-${var.venue}-${var.counter}-staging"
-  force_destroy = true
-  #   tags = {
-  #     Name        = "My bucket"
-  #     Environment = "Dev"
-  #   }
-}
-resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket" "s3_bucket" {
+#   bucket        = "${var.project}-${var.venue}-${var.counter}-staging"
+#   force_destroy = true
+#   #   tags = {
+#   #     Name        = "My bucket"
+#   #     Environment = "Dev"
+#   #   }
+# }
+# resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+#   bucket = aws_s3_bucket.s3_bucket.id
+#   acl    = "private"
+# }
 
-resource "aws_s3_object" "source_files" {
-  for_each = fileset(local.source_s3_directory, "**/*.*")
-  bucket   = aws_s3_bucket.s3_bucket.id
-  key      = each.value
-  source   = "${local.source_s3_directory}/${each.value}"
-  etag     = filemd5("${local.source_s3_directory}/${each.value}")
-  depends_on = [
-    aws_s3_bucket_acl.s3_bucket_acl
-  ]
-}
+# resource "aws_s3_object" "source_files" {
+#   for_each = fileset(local.source_s3_directory, "**/*.*")
+#   bucket   = aws_s3_bucket.s3_bucket.id
+#   key      = each.value
+#   source   = "${local.source_s3_directory}/${each.value}"
+#   etag     = filemd5("${local.source_s3_directory}/${each.value}")
+#   depends_on = [
+#     aws_s3_bucket_acl.s3_bucket_acl
+#   ]
+# }
