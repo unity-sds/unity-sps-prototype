e25684934be514e2031b3c1dc80b25fd943059b3
Drew Meyers <drew.meyers@jpl.nasa.gov>
2023-02-28 17:08:22 -0800

    Place ELB in front of Mozart and GRQ ES

--- terraform-unity/outputs.tf
+++ terraform-unity/outputs.tf
@@ -1,4 +1,8 @@
 output "load_balancer_hostnames" {
   description = "Load Balancer Ingress Hostnames"
-  value       = module.unity-sps-hysds-cluster.load_balancer_hostnames
+  value = {
+    mozart_es = module.unity-sps-hysds-cluster.load_balancer_hostnames.mozart_es
+    grq_es    = module.unity-sps-hysds-cluster.load_balancer_hostnames.grq_es
+    ades_wpst = module.unity-sps-hysds-cluster.load_balancer_hostnames.ades_wpst_api
+  }
 }
