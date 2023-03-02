a8d1a74d1dce332fab67f84c26c322c87cff8c7e
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2023-03-01 16:07:25 -0800

    add sps-api to terraform modules

---
+++ terraform-modules/terraform-unity-sps-hysds-cluster/sps_api.tf
@@ -0,0 +1,57 @@
+resource "kubernetes_service" "sps-api-service" {
+  metadata {
+    name      = "sps-api"
+    namespace = kubernetes_namespace.unity-sps.metadata[0].name
+  }
+  spec {
+    selector = {
+      app = "sps-api"
+    }
+    session_affinity = var.deployment_environment != "local" ? null : "ClientIP"
+    type             = var.service_type
+    port {
+      protocol    = "TCP"
+      port        = var.service_port_map.sps_api_api_service
+      target_port = 80
+      node_port   = var.service_type != "NodePort" ? null : var.node_port_map.sps_api_api_service
+    }
+  }
+}
+
+resource "kubernetes_deployment" "sps-api" {
+  metadata {
+    name      = "sps-api"
+    namespace = kubernetes_namespace.unity-sps.metadata[0].name
+    labels = {
+      app = "sps-api"
+    }
+  }
+
+  spec {
+    # replicas = 2
+    selector {
+      match_labels = {
+        app = "sps-api"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "sps-api"
+        }
+      }
+      spec {
+        container {
+          image             = var.docker_images.sps_api
+          image_pull_policy = "Always"
+          name              = "sps-api"
+          port {
+            container_port = 80
+          }
+        }
+        restart_policy = "Always"
+      }
+    }
+  }
+}
