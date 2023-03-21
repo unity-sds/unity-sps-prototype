b613942625eb399fb90d9d99d4f407690fbc558f
ryanhunter-jpl <ryan.g.hunter@jpl.nasa.gov>
2023-03-21 23:58:15 +0000

    replace hard coded venue with variable reference

--- terraform-modules/terraform-unity-sps-hysds-cluster/api_gateway.tf
+++ terraform-modules/terraform-unity-sps-hysds-cluster/api_gateway.tf
@@ -1,19 +1,19 @@
 # https://github.com/unity-sds/unity-cs-infra/blob/main/terraform-api-gateway-cognito/post-deployment/update-api-gateway-urls.sh
 data "aws_ssm_parameter" "api_gateway_rest_api_id" {
-  # name = format("/unity/dev/%s-%s/api-gateway/rest-api-id", var.namespace, var.counter)
-  name = "/unity/dev/unity-sps-1/api-gateway/rest-api-id"
+  # name = format("/unity/${var.venue}/%s-%s/api-gateway/rest-api-id", var.namespace, var.counter)
+  name = "/unity/${var.venue}/unity-sps-1/api-gateway/rest-api-id"
 }
 
 # Breakout region into a variable
 resource "null_resource" "update_api_gateway_stage_variables" {
   provisioner "local-exec" {
     command = <<-EOT
-      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "test" --region us-west-2 --patch-operations op=replace,path=/variables/adesWpstUrl,value="${kubernetes_service.ades-wpst-api-service.status[0].load_balancer[0].ingress[0].hostname}"
-      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "test" --region us-west-2 --patch-operations op=replace,path=/variables/grqEsUrl,value="${data.kubernetes_service.grq-es.status[0].load_balancer[0].ingress[0].hostname}"
-      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "test" --region us-west-2 --patch-operations op=replace,path=/variables/grqRestApiUrl,value="-"
-      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "test" --region us-west-2 --patch-operations op=replace,path=/variables/hysdsUiUrl,value="-"
-      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "test" --region us-west-2 --patch-operations op=replace,path=/variables/mozartEsUrl,value="${data.kubernetes_service.mozart-es.status[0].load_balancer[0].ingress[0].hostname}"
-      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "test" --region us-west-2 --patch-operations op=replace,path=/variables/mozartRestApiUrl,value="-"
+      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "${var.venue}" --region us-west-2 --patch-operations op=replace,path=/variables/adesWpstUrl,value="${kubernetes_service.ades-wpst-api-service.status[0].load_balancer[0].ingress[0].hostname}"
+      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "${var.venue}" --region us-west-2 --patch-operations op=replace,path=/variables/grqEsUrl,value="${data.kubernetes_service.grq-es.status[0].load_balancer[0].ingress[0].hostname}"
+      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "${var.venue}" --region us-west-2 --patch-operations op=replace,path=/variables/grqRestApiUrl,value="-"
+      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "${var.venue}" --region us-west-2 --patch-operations op=replace,path=/variables/hysdsUiUrl,value="-"
+      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "${var.venue}" --region us-west-2 --patch-operations op=replace,path=/variables/mozartEsUrl,value="${data.kubernetes_service.mozart-es.status[0].load_balancer[0].ingress[0].hostname}"
+      aws apigateway update-stage --rest-api-id "${data.aws_ssm_parameter.api_gateway_rest_api_id.value}" --stage-name "${var.venue}" --region us-west-2 --patch-operations op=replace,path=/variables/mozartRestApiUrl,value="-"
     EOT
   }
 }
