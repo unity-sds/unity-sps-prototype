f82b1fc0ddf7d57a248e42b2fe3c4c04dd8be44b
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2023-03-31 10:10:03 -0700

    use terraform modules for api gw integration

--- terraform-modules/terraform-unity-sps-hysds-cluster/api_gateway.tf
+++ terraform-modules/terraform-unity-sps-hysds-cluster/api_gateway.tf
@@ -4,25 +4,44 @@ data "aws_ssm_parameter" "api_gateway_rest_api_id" {
   name = "/unity/${var.venue}/api-gateway/rest-api-id"
 }
 
-# API spec template populated with domains through url
-data "template_file" "api_template"{
-  template = "${file(var.open_api_spec_file)}"
+data "aws_ssm_parameter" "api_gateway_rest_api_root_resource_id" {
+  # name = format("/unity/${var.venue}/%s-%s/api-gateway/rest-api-id", var.namespace, var.counter)
+  name = "/unity/${var.venue}/api-gateway/rest-api-root-resource-id"
+}
 
-  vars = {
-    adesWpstUri = "${kubernetes_service.ades-wpst-api-service.status[0].load_balancer[0].ingress[0].hostname[0]}:${var.service_port_map.ades_wpst_api_service}",
-    grqEsUri = "${kubernetes_service.grq-es.status[0].load_balancer[0].ingress[0].hostname[0]}:${var.service_port_map.grq2_es}",
-    grqRestApiUri = "-",
-    hysdsUiiUri = "-",
-    mozartEsUri = "${kubernetes_service.mozart-es.status[0].load_balancer[0].ingress[0].hostname[0]}:${var.service_port_map.mozart_es}",
-    mozartRestApiUri = "-"
+data "aws_ssm_parameter" "api_gateway_rest_api_lambda_authorizer_id" {
+  # name = format("/unity/${var.venue}/%s-%s/api-gateway/rest-api-id", var.namespace, var.counter)
+  name = "/unity/${var.venue}/api-gateway/rest-api-lambda-authorizer-id"
+}
+
+resource "aws_api_gateway_resource" "api_gateway_ades_wpst_resource" {
+  rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id.value
+  parent_id   = data.aws_ssm_parameter.api_gateway_rest_api_root_resource_id.value
+  path_part   = "ades-wpst"
+}
+
+resource "aws_api_gateway_resource" "api_gateway_ades_wpst_proxy_resource" {
+  rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id.value
+  parent_id   = aws_api_gateway_resource.api_gateway_ades_wpst_resource.id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "api_gateway_ades_wpst_proxy_method" {
+  rest_api_id   = data.aws_ssm_parameter.api_gateway_rest_api_id.value
+  resource_id   = aws_api_gateway_resource.api_gateway_ades_wpst_proxy_resource.id
+  http_method   = "ANY"
+  authorization = "CUSTOM"
+  authorizer_id = data.aws_ssm_parameter.api_gateway_rest_api_root_resource_id.value 
+
+  request_parameters = {
+    "method.request.path.proxy" = true
   }
 }
 
-# Breakout region into a variable
-resource "null_resource" "update_api_gateway_stage_variables" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      aws apigateway put-rest-api --rest-api-id ${data.aws_ssm_parameter.api_gateway_rest_api_id} --body "${data.template_file.api_template.rendered}" --mode merge --region ${var.region} --cli-binary-format raw-in-base64-out
-    EOT
-  }
+resource "aws_api_gateway_integration" "api_gateway_ades_wpst_proxy_integration" {
+  rest_api_id   = data.aws_ssm_parameter.api_gateway_rest_api_id.value
+  resource_id   = aws_api_gateway_resource.api_gateway_ades_wpst_proxy_resource.id
+  http_method   = aws_api_gateway_method.api_gateway_ades_wpst_proxy_method.http_method
+  type          = "HTTP_PROXY"
+  uri           = "${kubernetes_service.ades-wpst-api-service.status[0].load_balancer[0].ingress[0].hostname[0]}:${var.service_port_map.ades_wpst_api_service}"
 }
