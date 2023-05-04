f03b4eddd8003c60308ec27cf46d8089f42c876d
Drew Meyers <drew.meyers@jpl.nasa.gov>
2023-05-04 16:03:07 -0700

    Commented WPS-T ELB implementation

--- terraform-modules/terraform-unity-sps-hysds-cluster/api_gatway.tf
+++ terraform-modules/terraform-unity-sps-hysds-cluster/api_gatway.tf
@@ -1,7 +1,8 @@
 resource "aws_ssm_parameter" "update_ades_wpst_url_stage_variable_of_api_gateway" {
-  name       = format("/%s/%s/%s-%s/api-gateway/stage-variables/ades-wpst-url", var.project, var.venue, var.namespace, var.counter)
-  type       = "String"
-  value      = "${kubernetes_service.ades-wpst-api-service.status[0].load_balancer[0].ingress[0].hostname}:${var.service_port_map.ades_wpst_api_service}"
+  name  = format("/%s/%s/%s-%s/api-gateway/stage-variables/ades-wpst-url", var.project, var.venue, var.namespace, var.counter)
+  type  = "String"
+  value = "${kubernetes_service.ades-wpst-api-service.status[0].load_balancer[0].ingress[0].hostname}:${var.service_port_map.ades_wpst_api_service}"
+  # value      = "${aws_elb.ades_wpst_api_elb.dns_name}:${var.service_port_map.ades_wpst_api_service}"
   overwrite  = true
   depends_on = [kubernetes_service.ades-wpst-api-service]
   tags = merge(local.common_tags, {
