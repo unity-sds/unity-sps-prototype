897834c4b34cc0f6b11755d7c380eaa4b8fe2555
Drew Meyers <drew.meyers@jpl.nasa.gov>
2023-05-04 16:04:54 -0700

    Commented WPS-T ELB implementation

--- terraform-modules/terraform-unity-sps-hysds-cluster/outputs.tf
+++ terraform-modules/terraform-unity-sps-hysds-cluster/outputs.tf
@@ -18,6 +18,7 @@ output "load_balancer_hostnames" {
     mozart_es     = data.kubernetes_service.mozart-es.status[0].load_balancer[0].ingress[0].hostname,
     grq_es        = data.kubernetes_service.grq-es.status[0].load_balancer[0].ingress[0].hostname,
     ades_wpst_api = kubernetes_service.ades-wpst-api-service.status[0].load_balancer[0].ingress[0].hostname,
-    sps_api       = kubernetes_service.sps-api-service.status[0].load_balancer[0].ingress[0].hostname,
+    # ades_wpst_api = aws_elb.ades_wpst_api_elb.dns_name
+    sps_api = kubernetes_service.sps-api-service.status[0].load_balancer[0].ingress[0].hostname,
   }
 }
