6376631616720daf1f970bfbc5003cf4f00d8598
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2023-05-30 11:54:50 -0700

    add option to use vars instead of ssm params

--- terraform-unity/ssm_params.tf
+++ terraform-unity/ssm_params.tf
@@ -12,25 +12,31 @@ data "aws_ssm_parameter" "uds_dapa_api" {
 }
 
 data "aws_ssm_parameter" "default_group_node_group_launch_template_name" {
+  count = var.default_group_node_group_launch_template_name != null ? 1 : 0
   name = "/unity/extensions/eks/${var.eks_cluster_name}/nodeGroups/default/launchTemplateName"
 }
 
 data "aws_ssm_parameter" "default_node_group_name" {
+  count = var.default_node_group_name != null ? 1 : 0
   name = "/unity/extensions/eks/${var.eks_cluster_name}/nodeGroups/default/name"
 }
 
 data "aws_ssm_parameter" "eks_private_subnets" {
+  count = var.eks_private_subnets != null ? 1 : 0
   name = "/unity/extensions/eks/${var.eks_cluster_name}/networking/subnets/privateIds"
 }
 
 data "aws_ssm_parameter" "uads_development_efs_fsmt_id" {
+  count = var.uads_development_efs_fsmt_id != null ? 1 : 0
   name = "/unity/ads/efs/development/fsmtId"
 }
 
 data "aws_ssm_parameter" "account_venue" {
+  count = var.venue != null ? 1 : 0
   name = "/unity/account/venue"
 }
 
 data "aws_ssm_parameter" "account_project" {
+  count = var.project != null ? 1 : 0
   name = "/unity/account/project"
 }
