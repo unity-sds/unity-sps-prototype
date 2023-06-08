7ee7d53355a60513113264dc3a6009bf82d64e64
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2023-06-08 10:54:31 -0700

    optional ingest of apigateway ssm params

--- terraform-modules/terraform-unity-sps-hysds-cluster/api_gateway.tf
+++ terraform-modules/terraform-unity-sps-hysds-cluster/api_gateway.tf
@@ -1,39 +1,42 @@
 # Rest API ID from project api gateway deployment, needed to add resources, methods, and integrations to api gateway
 data "aws_ssm_parameter" "api_gateway_rest_api_id" {
+  count       = var.add_routes_to_api_gateway ? 1 : 0
   name = "/unity/cs/routing/api-gateway/rest-api-id"
 }
 
 # Rest API root resource ID from project api gateway deployment, needed by child resources
 data "aws_ssm_parameter" "api_gateway_rest_api_root_resource_id" {
+  count       = var.add_routes_to_api_gateway ? 1 : 0
   name = "/unity/${var.venue}/api-gateway/rest-api-root-resource-id"
 }
 
 # Lambda authorizer ID in Rest API, needed by methods authorizing with CS custom authorizer
 data "aws_ssm_parameter" "api_gateway_rest_api_lambda_authorizer_id" {
+  count       = var.add_routes_to_api_gateway ? 1 : 0
   name = "/unity/${var.venue}/api-gateway/rest-api-lambda-authorizer-id"
 }
 
 resource "aws_api_gateway_resource" "api_gateway_ades_wpst_resource" {
   count       = var.add_routes_to_api_gateway ? 1 : 0
-  rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id.value
-  parent_id   = data.aws_ssm_parameter.api_gateway_rest_api_root_resource_id.value
+  rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id[0].value
+  parent_id   = data.aws_ssm_parameter.api_gateway_rest_api_root_resource_id[0].value
   path_part   = "ades-wpst"
 }
 
 resource "aws_api_gateway_resource" "api_gateway_ades_wpst_proxy_resource" {
   count       = var.add_routes_to_api_gateway ? 1 : 0
-  rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id.value
+  rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id[0].value
   parent_id   = aws_api_gateway_resource.api_gateway_ades_wpst_resource[0].id
   path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "api_gateway_ades_wpst_proxy_method" {
   count         = var.add_routes_to_api_gateway ? 1 : 0
-  rest_api_id   = data.aws_ssm_parameter.api_gateway_rest_api_id.value
+  rest_api_id   = data.aws_ssm_parameter.api_gateway_rest_api_id[0].value
   resource_id   = aws_api_gateway_resource.api_gateway_ades_wpst_proxy_resource[0].id
   http_method   = "ANY"
   authorization = "CUSTOM"
-  authorizer_id = data.aws_ssm_parameter.api_gateway_rest_api_lambda_authorizer_id.value
+  authorizer_id = data.aws_ssm_parameter.api_gateway_rest_api_lambda_authorizer_id[0].value
 
   request_parameters = {
     "method.request.path.proxy" = true
@@ -42,7 +45,7 @@ resource "aws_api_gateway_method" "api_gateway_ades_wpst_proxy_method" {
 
 resource "aws_api_gateway_integration" "api_gateway_ades_wpst_proxy_integration" {
   count                   = var.add_routes_to_api_gateway ? 1 : 0
-  rest_api_id             = data.aws_ssm_parameter.api_gateway_rest_api_id.value
+  rest_api_id             = data.aws_ssm_parameter.api_gateway_rest_api_id[0].value
   resource_id             = aws_api_gateway_resource.api_gateway_ades_wpst_proxy_resource[0].id
   http_method             = aws_api_gateway_method.api_gateway_ades_wpst_proxy_method[0].http_method
   integration_http_method = "ANY"
@@ -96,14 +99,14 @@ resource "aws_api_gateway_integration" "api_gateway_sps_api_proxy_integration" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/api_gateway_deployment#triggers
 resource "aws_api_gateway_deployment" "api_gateway_deployment" {
   count       = var.add_routes_to_api_gateway ? 1 : 0
-  rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id.value
+  rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id[0].value
   lifecycle {
     create_before_destroy = true
   }
   triggers = {
     region      = var.region
     venue       = var.venue
-    rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id.value
+    rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id[0].value
     redployment = sha1(jsonencode([
       aws_api_gateway_resource.api_gateway_ades_wpst_resource[0]
     ]))
@@ -133,6 +136,6 @@ EOF
 resource "null_resource" "api_gateway_stage_update_resource" {
   count = var.add_routes_to_api_gateway ? 1 : 0
   provisioner "local-exec" {
-    command = "aws apigateway update-stage --region ${var.region} --rest-api-id ${data.aws_ssm_parameter.api_gateway_rest_api_id.value} --stage-name=${var.venue} --patch-operations op='replace',path='/deploymentId',value='${aws_api_gateway_deployment.api_gateway_deployment[0].id}'"
+    command = "aws apigateway update-stage --region ${var.region} --rest-api-id ${data.aws_ssm_parameter.api_gateway_rest_api_id[0].value} --stage-name=${var.venue} --patch-operations op='replace',path='/deploymentId',value='${aws_api_gateway_deployment.api_gateway_deployment[0].id}'"
   }
 }
