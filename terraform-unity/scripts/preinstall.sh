36c384127b7e5933ef60793b93a32756dfe980cc
Tom Barber <tom@spicule.co.uk>
2023-09-28 16:17:05 +0100

    Update preinstall.sh

--- terraform-unity/scripts/preinstall.sh
+++ terraform-unity/scripts/preinstall.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+LOGFILE="/tmp/scriptlog.log"
+exec > $LOGFILE 2>&1
+set -x
 mkdir $WORKDIR/k8s
-aws eks update-kubeconfig --name $EKS_NAME --kubeconfig $WORKDIR/k8s/kubernetes.yml
+aws eks update-kubeconfig --name $UNITY_EKS --kubeconfig $WORKDIR/k8s/kubernetes.yml
