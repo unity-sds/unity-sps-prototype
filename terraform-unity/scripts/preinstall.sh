7bc78118f41478367c2c053b7efabafa0d7571e3
Barber <barber@MT-306070.jpl.nasa.gov>
2023-10-04 08:52:06 +0100

    update var

--- terraform-unity/scripts/preinstall.sh
+++ terraform-unity/scripts/preinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir $WORKDIR/k8s
-aws eks update-kubeconfig --name $EKS_NAME --kubeconfig $WORKDIR/k8s/kubernetes.yml
+aws eks update-kubeconfig --name $UNITY_EKS --kubeconfig $WORKDIR/k8s/kubernetes.yml
