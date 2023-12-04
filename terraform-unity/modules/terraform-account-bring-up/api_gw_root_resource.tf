3bac15a2e05a49e1924db88d62f374f011747955
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2023-12-04 10:56:38 -0800

    add account bring up module with api gw root resource

---
+++ terraform-unity/modules/terraform-account-bring-up/api_gw_root_resource.tf
@@ -0,0 +1,22 @@
+# Rest API ID from project api gateway deployment, needed to add resources, methods, and integrations to api gateway
+data "aws_ssm_parameter" "api_gateway_rest_api_id" {
+  name  = "/unity/cs/routing/api-gateway/rest-api-id-2"
+}
+
+# Rest API root resource ID from project api gateway deployment, needed by child resources
+data "aws_api_gateway_resource" "api_gateway_rest_api_root_resource" {
+    rest_api_id = aws_ssm_parameter.api_gateway_rest_api_id.value
+    path = "/"    
+}
+
+resource "aws_api_gateway_resource" "api_gateway_sps_resource" {
+  rest_api_id = data.aws_ssm_parameter.api_gateway_rest_api_id[0].value
+  parent_id   = data.aws_api_gateway_resource.api_gateway_rest_api_root_resource.id
+  path_part   = "sps"
+}
+
+resource "aws_ssm_parameter" "api_gateway_sps_path_resource_id" {
+    name = "/unity/${var.service_area}/api-gateway/sps_resource_id"
+    type = "String"
+    value = aws_api_gateway_resource.api_gateway_sps_resource.id
+}
