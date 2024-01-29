28fe01b7364915536c20f0e8a0503eeed797e10f
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2024-01-29 11:07:06 -0800

    add validation script for eks deployments

---
+++ terraform-unity/modules/terraform-eks-cluster/validate.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#### validate terraform and deploy
+
+terraform fmt
+
+CLUSTER_NAME=$(openssl rand -base64 12)
+
+terraform workspace new $CLUSTER_NAME
+
+terraform init
+
+terraform apply -auto-approve -var "cluster_name=$CLUSTER_NAME"
+
+#### validate k8s deployment
+
+aws eks update-kubeconfig --region us-west-2 --name $CLUSTER_NAME --kubeconfig ./temp_kube_cfg
+
+OLD_CFG=$(echo $KUBECONFIG)
+export KUBECONFIG=./temp_kube_cfg
+
+# validates that the API is reachable and user has permissions
+kubectl get all -A
+
+rm $KUBECONFIG
+export KUBECONFIG=$OLD_CFG
