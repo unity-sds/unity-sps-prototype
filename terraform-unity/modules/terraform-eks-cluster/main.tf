d85edf4c1e44da22e7dc35caa4a1b67639b78df3
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2024-01-31 15:44:22 -0800

    add more permissions to eks node group and change eks module branch

--- terraform-unity/modules/terraform-eks-cluster/main.tf
+++ terraform-unity/modules/terraform-eks-cluster/main.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 module "unity-eks" {
-  source          = "git@github.com:unity-sds/unity-cs-infra.git//terraform-unity-eks_module"
+  source          = "git@github.com:unity-sds/unity-cs-infra.git//terraform-unity-eks_module?ref=addons_and_output"
   deployment_name = var.cluster_name
 
   nodegroups = {
@@ -20,6 +20,42 @@ module "unity-eks" {
 
   cluster_version = "1.27"
 }
+
+# add extra policies as inline policy
+resource "aws_iam_role_policy" "sps_airflow_eks_inline_policy" {
+  name   = "sps_airflow_eks_inline_policy"
+  role   = module.unity-eks.cluster_iam_role
+  policy = <<EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Statement1",
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:AddTags",
+                "elasticloadbalancing:Describe*",
+                "elasticloadbalancing:ConfigureHealthCheck",
+                "elasticloadbalancing:CreateLoadBalancer",
+                "elasticloadbalancing:DeleteLoadBalancer",
+                "elasticloadbalancing:ModifyLoadBalancerAttributes",
+                "elasticloadbalancing:RegisterTargets",
+                "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+                "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+                "elasticloadbalancing:DeregisterTargets",
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:AuthorizeSecurityGroupEgress",
+                "ec2:CreateSecurityGroup",
+                "ec2:Describe*",
+                "ec2:RevokeSecurityGroupIngress"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOT
+}
+
 
 data "aws_eks_cluster" "cluster" {
   name       = var.cluster_name
--- terraform-unity/modules/terraform-eks-cluster/variables.tf
+++ terraform-unity/modules/terraform-eks-cluster/variables.tf
@@ -2,3 +2,8 @@ variable "cluster_name" {
   type    = string
   default = "sps-processing-cluster"
 }
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
