ff0d2993ca9c62cc165c5146680d083e366447c8
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-02-02 09:59:22 -0700

    Working DAG that passes CWL parameters to the KPO.

--- airflow/dags/docker_cwl_kpo.py
+++ airflow/dags/docker_cwl_kpo.py
@@ -6,7 +6,8 @@ from airflow.operators.bash import BashOperator
 from airflow.models.param import Param
 import uuid
 
-default_cwl_workflow = "https://raw.githubusercontent.com/unity-sds/unity-sps-prototype/cwl-docker/cwl/cwl_workflows/hello_world_from_docker.cwl"
+default_cwl_workflow = "https://raw.githubusercontent.com/unity-sds/unity-sps-prototype/cwl-docker/cwl/cwl_workflows/echo_from_docker.cwl"
+default_args_as_json = "{}"
 
 # Default DAG configuration
 dag_default_args = {
@@ -16,7 +17,7 @@ dag_default_args = {
 }
 
 # The DAG
-dag = DAG(dag_id='docker-cwl-kpo',
+dag = DAG(dag_id='docker-cwl-dag',
           description='Kubernetes Pod Oprator to execute a CWL workflow with docker-in-docker',
           tags=['cwl', 'unity-sps', "docker"],
           is_paused_upon_creation=True,
@@ -26,6 +27,7 @@ dag = DAG(dag_id='docker-cwl-kpo',
           default_args=dag_default_args,
           params={
             "cwl_workflow": Param(default_cwl_workflow, type="string"),
+            "args_as_json": Param(default_args_as_json, type="string"),
           })
 
 # Environment variables
@@ -54,7 +56,7 @@ cwl_task = KubernetesPodOperator(
     task_id=job_name,
     full_pod_spec=full_pod_spec,
     pod_template_file="/opt/airflow/dags/docker_cwl_pod.yaml",
-    arguments=["{{ params.cwl_workflow }}"],
+    arguments=["{{ params.cwl_workflow }}", "{{ params.args_as_json }}"],
     dag=dag)
 
 stage_out_task = BashOperator(
--- cwl/docker_cwl_entrypoint.sh
+++ cwl/docker_cwl_entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 # Script to execute a CWL workflow that includes Docker containers
 # The Docker engine is started before the CWL execution, and stopped afterwards.
+# $1: the CWL workflow URL (example: https://raw.githubusercontent.com/unity-sds/unity-sps-prototype/cwl-docker/cwl/cwl_workflows/echo_from_docker.cwl)
+# $2: the CWL job parameters as a JSON fomatted string (example: { name: John Doe })
 set -ex
 cwl_workflow=$1
-echo "Executing CWL workflow: $cwl_workflow"
+job_args=$2
+echo "Executing CWL workflow: $cwl_workflow with json arguments: $job_args"
+echo $job_args > /tmp/job_args.json
+cat /tmp/job_args.json
 
 # Start Docker engine
 dockerd &> dockerd-logfile &
@@ -18,7 +23,7 @@ done
 # Execute CWL workflow
 # docker run hello-world
 source /usr/share/cwl/venv/bin/activate
-cwl-runner $cwl_workflow
+cwl-runner $cwl_workflow /tmp/job_args.json
 deactivate
 
 # Stop Docker engine
